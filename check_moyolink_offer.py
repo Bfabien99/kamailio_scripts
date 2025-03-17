d44e08bb0954372cdb8c08526400ff6d65c50850
bfabien99 <fabienbrou99@gmail.com>
2025-03-17 15:41:17 +0000

    recupération des offres

---
+++ check_moyolink_offer.py
@@ -0,0 +1,20 @@
+from configs import connection_url, sys, psycopg
+
+try:
+    ## recupération de l'id de l'offre
+    offer_id = str(sys.argv[1])
+    if not offer_id:
+        print("False")
+    
+    ## recupération de l'id et du price de l'offer
+    with psycopg.connect(connection_url) as conn:
+        with conn.cursor() as cur:
+            cur.execute("SELECT id, price FROM offers WHERE id=%s", (offer_id,))
+            record = cur.fetchone()
+            
+            if(record):
+                print("True")
+            else:
+                print("False")
+except Exception as e:
+    print("False")
