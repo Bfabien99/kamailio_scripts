de2a7cb33cb70dfd766871128b7a94bf29f431ee
bfabien99 <fabienbrou99@gmail.com>
2025-03-17 15:41:58 +0000

    vérification du code de l'app

---
+++ check_moyolink_code.py
@@ -0,0 +1,20 @@
+from configs import connection_url, sys, psycopg
+
+try:
+    customer_id = str(sys.argv[1])
+    password = str(sys.argv[2])
+    if not customer_id or not password:
+        print("False")
+    
+    
+    with psycopg.connect(connection_url) as conn:
+        with conn.cursor() as cur:
+            cur.execute("SELECT * FROM subscriber WHERE customerId=%s and password=%s", (customer_id, password))
+            record = cur.fetchone()
+            
+            if(record):
+                print("True")
+            else:
+                print("False")
+except Exception as e:
+    print("False")
