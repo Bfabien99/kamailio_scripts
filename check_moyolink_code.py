6bc03977b2963da2aa8627c6954b04094c50ba5f
bfabien99 <fabienbrou99@gmail.com>
2025-03-18 15:36:45 +0000

    refactorisation afin que le module exec_avp de kamailio puisse conserver les valeurs de retour

--- check_moyolink_code.py
+++ check_moyolink_code.py
@@ -2,53 +2,26 @@ from configs import connection_url
 import sys
 import psycopg
 
-from custom_exception import MissingDataError, InvalidDataError
-
 from helpers import make_log
 
+try:
+    customer_id = str(sys.argv[1])
+    password = str(sys.argv[2])
 
-def check_moyolink_code(customer_id, password) -> dict:
-    """
-    Vérifie si le couple (customer_id, password) est valide.
-    
-    :param customer_id: L'id du customer.
-    :param password: Le mot de passe de l'application.
-    """
-    try:
-        if not customer_id or not password:
-            raise MissingDataError("CustomerId ou password absent")
+    if not customer_id or not password:
+        print("success='False';message='CustomerId ou password absent'")
 
-        with psycopg.connect(connection_url, row_factory=psycopg.rows.dict_row) as conn:
-            with conn.cursor() as cur:
-                cur.execute(
-                    "SELECT * FROM call_verifications WHERE 'customerId'=%s and password=%s",
-                    (customer_id, password),
-                )
-                record = cur.fetchone()
+    with psycopg.connect(connection_url, row_factory=psycopg.rows.dict_row) as conn:
+        with conn.cursor() as cur:
+            cur.execute(
+                "SELECT * FROM call_verifications WHERE 'customerId'=%s and password=%s",  # Ajustez la syntaxe de la requête
+                (customer_id, password),
+            )
+            record = cur.fetchone()
 
-                if record:
-                    return {"success": True, "data": {"code": record}}
-                else:
-                    raise InvalidDataError("Le code de l'application est invalide")
-    except Exception as e:
-        raise e
-
-
-def main():
-    try:
-        customer_id = str(sys.argv[1])
-        password = str(sys.argv[2])
-
-        check_moyolink_code(customer_id, password)
-    except MissingDataError as e:
-        make_log(str(e), 4)
-        return {"success": False, "message": str(e)}
-    except InvalidDataError as e:
-        make_log(str(e), 4)
-        return {"success": False, "message": str(e)}
-    except Exception as e:
-        make_log(str(e), 4)
-        return {"success": False, "message": str(e)}
-
-
-print(main())
+            if record:
+                print(f"success='True';message='{record['password']}'")
+            else:
+                print("success='False';message='application code is invalid'")
+except Exception as e:
+    print(f"success='False';message='{str(e)}'")
